a79c66264b58cdec5c4ea9b1326ee75655254fc8
tommyy1708 <hzhan081@fiu.edu>
2024-07-28 23:13:14 -0400

    add environments variables

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const express = require('express');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import dotenv from 'dotenv';
+import express from 'express';
+
 const app = express();
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.NODE_APP_APIKEY,
+  authDomain: process.env.NODE_APP_AUTHDOMAIN,
+  projectId: process.env.NODE_APP_PROJECTID,
+  storageBucket: process.env.NODE_APP_STORAGEBUKET,
+  messagingSenderId: process.env.NODE_APP_MESSAGESENDID,
+  appId: process.env.NODE_APP_APPID,
+  measurementId: process.env.NODE_APP_MESUREMENTID,
+};
+
+// Load service account credentials
+
+const admin = initializeApp(firebaseConfig);
 
 const PORT = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => res.send('Express on Vercel'));
-
-
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
-
-module.exports = app;
+export default app;
